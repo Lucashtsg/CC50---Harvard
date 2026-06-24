74bde6e53ebb4a0e7511dd1a46da3b86f328e46e
Lucas Henrique Silva Guimarães <126778448+Lucashtsg@users.noreply.github.com>
2026-06-24 16:18:58 -0300

    Finalizando o desafio

--- C/challenge mario/mario.c
+++ C/challenge mario/mario.c
@@ -4,25 +4,28 @@
 int main(void)
 {
     int height = get_int("Qual a altura que você deseja? ");
-
-    if (height > 1 && height < 10)
+    if (height >= 1 && height < 10)
     {
 
             for (int j = 0; j < height; j++)
             {
-
+                for(int space = 0; space < height - j - 1; space++)
+                {
+                    printf(" ");
+                }
                 int contador = 0;
                 do{
                     printf("#");
                     contador++;
-
-
                 }while(contador <= j);
                 printf("\n");
-
-
             }
 
 
+    }
+    else
+    {
+        printf("Coloque um numero de 1 a 10 \n");
+        return 0;
     }
 }
